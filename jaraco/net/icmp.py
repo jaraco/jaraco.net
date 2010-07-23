2a4d8b35e9f0a43ec581559a3892d96895080a6b
jaraco <none@none>
2010-07-23 19:33:47 +0000

    Added function wait_for_host to icmp lib

--- jaraco/net/icmp.py
+++ jaraco/net/icmp.py
@@ -4,6 +4,7 @@ import struct
 import time
 import operator
 import random
+from jaraco.util import QuickTimer
 
 def calculate_checksum(bytes):
 	r"""
@@ -35,8 +36,12 @@ def pack_echo_header(id, data, sequence=1):
 
 def ping(dest_addr, timeout = 2):
 	"""
+	Send an ICMP Echo request to a host and return how long it takes.
+	
+	Raise socket.timeout if no response is received within timeout.
+	
 	>>> ping('127.0.0.1')
-	received
+	datetime.timedelta(...)
 	"""
 	icmp_proto = socket.getprotobyname('icmp')
 	icmp_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp_proto)
@@ -48,14 +53,27 @@ def ping(dest_addr, timeout = 2):
 	packet = header + data
 	icmp_socket.sendto(packet, (dest_addr, icmp_port))
 	
+	timer = QuickTimer()
 	read_fs, write_fs, ex_fs = select.select([icmp_socket], [], [], timeout)
+	delay = timer.Stop()
 	if not read_fs:
-		print 'timed out'
-		return
+		raise socket.timeout('timed out')
+
 	packet, addr = icmp_socket.recvfrom(1024)
 	header = packet[20:28]
 	type, code, checksum, recv_id, sequence = struct.unpack('bbHHh', header)
-	if recv_id == id:
-		print 'received'
-	else:
-		print 'different id'
+	if recv_id != id:
+		raise socket.error('transmission failure ({recv_id} != {id})'.format(**vars()))
+	return delay
+
+def wait_for_host(host):
+	while True:
+		try:
+			ping(host)
+			break
+		except socket.error:
+			pass
+	import datetime
+	return datetime.datetime.now()
+
+		
