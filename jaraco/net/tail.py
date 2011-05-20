12afaca0cc9ca342ffb1a43c490a9122fb739945
Jason R. Coombs <jaraco@jaraco.com>
2011-05-20 19:14:28 -0400

    Another attempt to trap the SIGHUP and SIGTERM events to close the generators, but this technique does not work either

--- jaraco/net/tail.py
+++ jaraco/net/tail.py
@@ -1,6 +1,8 @@
-import cherrypy
 import sys
 import time
+import functools
+
+import cherrypy
 
 def tail_f(filename):
 	interval = 1.0
@@ -12,7 +14,6 @@ def tail_f(filename):
 			if not line:
 				time.sleep(interval)
 				file.seek(where)
-				yield '\n'
 			else:
 				yield line
 
@@ -36,7 +37,24 @@ class TailedFile(object):
 		cherrypy.response.stream = True
 		cherrypy.response.headers['content-type'] = 'text/plain'
 		cherrypy.request.source = tail_f(self.filename)
+		all_tails.append(cherrypy.request.source)
 		return cherrypy.request.source
 
+all_tails = []
+def close_all():
+	print("attempting to close all")
+	for tail in all_tails:
+		tail.close()
+
+def on_signal(signal_name, handler):
+	orig_handler = cherrypy.engine.signal_handler.handlers[signal_name]
+	@functools.wraps(orig_handler)
+	def wrapper():
+		handler()
+		orig_handler()
+	cherrypy.engine.signal_handler.handlers[signal_name] = wrapper
+
 if __name__ == '__main__':
+	on_signal('SIGHUP', close_all)
+	on_signal('SIGTERM', close_all)
 	cherrypy.quickstart(TailedFile(sys.argv[1]))
