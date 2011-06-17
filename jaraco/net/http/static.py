f1fc8c0323c4b0558108000996cbbcfd39230f97
Jason R. Coombs <jaraco@jaraco.com>
2011-06-17 16:03:42 -0400

    Added directory listing support to serve-local

--- jaraco/net/http/static.py
+++ jaraco/net/http/static.py
@@ -1,6 +1,9 @@
 import cherrypy
 import os
 
+from . import staticdirindex
+from . import htmldir
+
 def serve_local():
 	"""
 	Serve the current directory as static files.
@@ -10,9 +13,9 @@ def serve_local():
 			'server.socket_host': '::0',
 		},
 		'/': {
-			'tools.staticdir.root': os.getcwd(),
 			'tools.staticdir.on': 'true',
-			'tools.staticdir.dir': '.',
+			'tools.staticdir.indexlister': htmldir.htmldir,
+			'tools.staticdir.dir': os.getcwd(),
 		},
 	}
 	cherrypy.quickstart(None, config=config)
