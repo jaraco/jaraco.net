5211ff8a4e83911a878943425a96c879bb587df1
Jason R. Coombs <jaraco@jaraco.com>
2011-06-26 20:31:54 -0400

    Another attempt to stop tailing the files when the engine exits (using a Plugin). This one doesn't work either

--- jaraco/net/tail.py
+++ jaraco/net/tail.py
@@ -2,7 +2,7 @@ import sys
 import time
 import functools
 
-import cherrypy
+import cherrypy.process.plugins
 
 def tail_f(filename):
 	interval = 1.0
@@ -11,17 +11,12 @@ def tail_f(filename):
 		while True:
 			where = file.tell()
 			line = file.readline()
+			print 'read', line
 			if not line:
 				time.sleep(interval)
 				file.seek(where)
 			else:
 				yield line
-
-def close_source():
-	cherrypy.log.error("closing source")
-	cherrypy.request.source.close()
-
-cherrypy.tools.closer = cherrypy.Tool('on_end_request', close_source)
 
 class TailedFile(object):
 	"""
@@ -32,29 +27,30 @@ class TailedFile(object):
 		self.filename = filename
 
 	@cherrypy.expose
-	@cherrypy.tools.closer()
 	def index(self):
 		cherrypy.response.stream = True
 		cherrypy.response.headers['content-type'] = 'text/plain'
 		cherrypy.request.source = tail_f(self.filename)
-		all_tails.append(cherrypy.request.source)
+		cherrypy.engine.publish('register-tail')
 		return cherrypy.request.source
 
-all_tails = []
-def close_all():
-	print("attempting to close all")
-	for tail in all_tails:
-		tail.close()
+class TailTracker(cherrypy.process.plugins.SimplePlugin, list):
+	def __init__(self, bus):
+		self.bus = bus
+		self.bus.subscribe('register-tail', self.register)
 
-def on_signal(signal_name, handler):
-	orig_handler = cherrypy.engine.signal_handler.handlers[signal_name]
-	@functools.wraps(orig_handler)
-	def wrapper():
-		handler()
-		orig_handler()
-	cherrypy.engine.signal_handler.handlers[signal_name] = wrapper
+	def register(self):
+		self.append(cherrypy.request.source)
+
+	def stop(self):
+		# close all tails
+		for tail in self:
+			tail.close()
+	stop.priority = 100
+
+	def __hash__(self):
+		return hash(id(self))
 
 if __name__ == '__main__':
-	on_signal('SIGHUP', close_all)
-	on_signal('SIGTERM', close_all)
+	TailTracker(cherrypy.engine).subscribe()
 	cherrypy.quickstart(TailedFile(sys.argv[1]))
