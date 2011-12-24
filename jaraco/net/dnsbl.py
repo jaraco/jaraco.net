30c405023551f38e56b5f5e09dcaaa217f311868
Jason R. Coombs <jaraco@jaraco.com>
2011-12-24 00:23:58 -0500

    Refactored dnsbl module

--- jaraco/net/dnsbl.py
+++ jaraco/net/dnsbl.py
@@ -1,37 +1,58 @@
-#!python
+"""
+dnsbl: DNS blocklist support
+"""
 
-# $Id$
+from __future__ import print_function
 
 import sys
 import socket
+import argparse
 
-def reverse_ip(ip):
-	return '.'.join(reversed(ip.split('.')))
+class BlocklistHit(object):
+	def __init__(self, host, blocklist, result):
+		self.host = host
+		self.blocklist = blocklist
+		self.result = result
 
-blocklist_servers = [
+	def __str__(self):
+		return "{host} listed with {blocklist} as {result}".format(**vars(self))
+
+class Server(unicode):
+	@staticmethod
+	def reverse_ip(ip):
+		return '.'.join(reversed(ip.split('.')))
+
+	def lookup(self, host):
+		ip = socket.gethostbyname(host)
+		rev_ip = self.reverse_ip(ip)
+		key = '.'.join((rev_ip, self))
+		try:
+			res = socket.gethostbyname(key)
+			print(host, 'listed with', self, 'as', res)
+		except socket.gaierror:
+			return
+		return BlocklistHit(host, self, res)
+
+	@classmethod
+	def handle_command_line(cls):
+		parser = argparse.ArgumentParser()
+		parser.add_argument('host')
+		args = parser.parse_args()
+		cls.lookup_all(args.host)
+
+	@staticmethod
+	def lookup_all(host):
+		for server in blocklist_servers:
+			res = server.lookup(host)
+			if res: print(res)
+
+blocklist_servers = map(Server, [
 	'dnsbl.jaraco.com',
 	'zen.spamhaus.org',
 	'ips.backscatterer.org',
 	'bl.spamcop.net',
 	'list.dsbl.org',
-]
-
-def lookup_host(host):
-	ip = socket.gethostbyname(host)
-
-	result = False
-	for bl in (blocklist_servers):
-		lookup = '.'.join((reverse_ip(ip), bl))
-		try:
-			res = socket.gethostbyname(lookup)
-			print host, 'listed with', bl, 'as', res
-			result = True
-		except socket.gaierror:
-			pass
-	return result
-
-def handle_cmdline():
-	lookup_host(sys.argv[1])
+])
 
 if __name__ == '__main__':
-	handle_cmdline()
+	Server.handle_command_line()
