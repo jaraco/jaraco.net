77f889d753f7d25937118836d598d114d4e40c0d
Jason R. Coombs <jaraco@jaraco.com>
2012-03-30 20:59:13 -0400

    Added ability to specify directory to index

--- jaraco/net/site.py
+++ jaraco/net/site.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 
+import argparse
+
 from path import path
 from jaraco.util.string import local_format as lf
 
@@ -8,10 +10,14 @@ def make_index_cmd():
 	Generate an index file for each directory that itself has an
 	index.htm file
 	"""
-	root = path('.')
+	parser = argparse.ArgumentParser()
+	parser.add_argument('root', default=path('.'), type=path)
+	args = parser.parse_args()
 	print('<ul>')
-	for dir in sorted(root.dirs()):
-		if not (dir/'index.htm').exists(): continue
+	for dir in sorted(args.root.dirs()):
+		if not (
+			(dir/'index.htm').exists() or
+			(dir/'index.html').exists()): continue
 		dir = dir.basename()
 		print(lf('<li><a href="{dir}/index.htm">{dir}</a></li>'))
 	print('</ul>')
