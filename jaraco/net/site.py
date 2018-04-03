9e468b455b193b59602aee31e4c9585ffbae5942
Jason R. Coombs <jaraco@jaraco.com>
2018-04-02 22:11:44 -0400

    Delint

--- jaraco/net/site.py
+++ jaraco/net/site.py
@@ -5,6 +5,7 @@ import argparse
 from path import path
 from jaraco.text import local_format as lf
 
+
 def make_index_cmd():
 	"""
 	Generate an index file for each directory that itself has an
@@ -16,8 +17,9 @@ def make_index_cmd():
 	print('<ul>')
 	for dir in sorted(args.root.dirs()):
 		if not (
-			(dir/'index.htm').exists() or
-			(dir/'index.html').exists()): continue
+			(dir / 'index.htm').exists() or
+			(dir / 'index.html').exists()):
+			continue
 		dir = dir.basename()
 		print(lf('<li><a href="{dir}/index.htm">{dir}</a></li>'))
 	print('</ul>')
