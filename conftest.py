fc1851c7adcd8f072457f8b897595f2eef699265
Jason R. Coombs <jaraco@jaraco.com>
2019-03-03 09:38:07 -0500

    Move whois service for Windows to its own module

--- conftest.py
+++ conftest.py
@@ -3,4 +3,5 @@ import platform
 
 collect_ignore = [
 	'jaraco/net/dns.py',
+	'jaraco/net/whois_svc.py',
 ] if platform.system() != 'Windows' else []
--- jaraco/net/whois.py
+++ jaraco/net/whois.py
@@ -11,7 +11,6 @@ into a textual format.
 
 import os
 import re
-import sys
 import logging
 import abc
 import socket
@@ -22,20 +21,12 @@ from six.moves import urllib
 from six.moves import http_cookiejar
 from six.moves import socketserver
 
-import jaraco.logging
 try:
 	from ClientForm import ParseResponse, ItemNotFoundError
 except ImportError:
 	"Disabled for Python 3 compatibility"
 from bs4 import BeautifulSoup
 from jaraco.classes.meta import LeafClassesMeta
-
-try:
-	import win32service
-	import win32serviceutil
-	import servicemanager
-except ImportError:
-	pass
 
 from .http import mechanize
 
@@ -295,61 +286,6 @@ def serve():
 	init()
 	Listener().serve_forever()
 
-
-# On Windows, run as a service
-if 'win32serviceutil' in globals():
-	class Service(win32serviceutil.ServiceFramework):
-		_svc_name_ = 'whois_bridge'
-		_svc_display_name_ = 'Whois HTTP Bridge'
-
-		def __init__(self, args):
-			win32serviceutil.ServiceFramework.__init__(self, args)
-
-		def SvcStop(self):
-			self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
-			self.listener.server_close()
-
-		def SvcDoRun(self):
-			self._setup_logging()
-
-			log.info('%s service is starting.', self._svc_display_name_)
-			servicemanager.LogMsg(
-				servicemanager.EVENTLOG_INFORMATION_TYPE,
-				servicemanager.PYS_SERVICE_STARTED,
-				(self._svc_name_, '')
-			)
-
-			self.run()
-
-			servicemanager.LogMsg(
-				servicemanager.EVENTLOG_INFORMATION_TYPE,
-				servicemanager.PYS_SERVICE_STOPPED,
-				(self._svc_name_, '')
-			)
-			log.info('%s service is stopped.', self._svc_display_name_)
-
-		def run(self):
-			init()
-			self.listener = Listener()
-			self.listener.serve_until_closed()
-
-		def _setup_logging(self):
-			logfile = os.path.join(
-				os.environ['WINDIR'], 'system32', 'LogFiles',
-				self._svc_display_name_, 'events.log')
-			handler = jaraco.logging.TimestampFileHandler(logfile)
-			handlerFormat = '[%(asctime)s] - %(levelname)s - [%(name)s] %(message)s'
-			handler.setFormatter(logging.Formatter(handlerFormat))
-			logging.root.addHandler(handler)
-			# if I don't redirect stdoutput and stderr, when the stdio flushes,
-			#  an exception will be thrown and the service will bail
-			sys.stdout = jaraco.logging.LogFileWrapper('stdout')
-			sys.stderr = jaraco.logging.LogFileWrapper('stderr')
-			logging.root.level = logging.INFO
-
-		@classmethod
-		def handle_command_line(cls):
-			win32serviceutil.HandleCommandLine(cls)
 
 if __name__ == '__main__':
 	serve()
