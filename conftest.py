e618777e5fe4cf5ce145fc42b0498aa626edb34c
Jason R. Coombs <jaraco@jaraco.com>
2022-11-30 13:54:09 -0500

    Construct collect_ignore in one expression.

--- conftest.py
+++ conftest.py
@@ -2,18 +2,20 @@ import sys
 import platform
 
 
-windows_ignore = (
-    ['jaraco/net/dns.py', 'jaraco/net/whois_svc.py']
-    if platform.system() != 'Windows'
-    else []
+collect_ignore = (
+    [
+        'jaraco/net/devices/linux2.py',
+        'jaraco/net/devices/win32.py',
+    ]
+    + [
+        # modules only import on Windows
+        'jaraco/net/dns.py',
+        'jaraco/net/whois_svc.py',
+    ]
+    * (platform.system() != 'Windows')
+    + [
+        # fabric fails on Python 3.11
+        'fabfile.py',
+    ]
+    * (sys.version_info > (3, 11))
 )
-
-
-collect_ignore = [
-    'jaraco/net/devices/linux2.py',
-    'jaraco/net/devices/win32.py',
-] + windows_ignore
-
-
-# fabric fails on Python 3.11
-collect_ignore += ['fabfile.py'] * (sys.version_info > (3, 11))
