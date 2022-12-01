3c06f3b21a27b67efe6262b7dd3f7b5309e0e8d9
Jason R. Coombs <jaraco@jaraco.com>
2022-12-01 11:37:31 -0500

    Implement flushable shelves

--- jaraco/net/http/cookies.py
+++ jaraco/net/http/cookies.py
@@ -2,6 +2,32 @@ import http.cookiejar
 import shelve
 
 
+class FlushableShelf(shelve.DbfilenameShelf):
+    """
+    >>> fn = getfixture('tmp_path') / 'shelf'
+    >>> shelf = FlushableShelf(fn)
+    >>> shelf['foo'] = 'bar'
+    >>> shelf.flush()
+    >>> copy = FlushableShelf(fn)
+    >>> copy['foo']
+    'bar'
+    >>> shelf['bar'] = 'baz'
+    >>> shelf.flush()
+    >>> FlushableShelf(fn)['bar']
+    'baz'
+    """
+
+    def __init__(self, filename, *args, **kwargs):
+        self.filename = filename
+        self.args = args
+        self.kwargs = kwargs
+        super().__init__(filename, *args, **kwargs)
+
+    def flush(self):
+        self.close()
+        super().__init__(self.filename, *self.args, **self.kwargs)
+
+
 class ShelvedCookieJar(http.cookiejar.CookieJar):
     """
     Cookie jar backed by a shelf.
@@ -9,9 +35,9 @@ class ShelvedCookieJar(http.cookiejar.CookieJar):
     Automatically persists cookies to disk.
     """
 
-    def __init__(self, shelf: shelve.Shelf, **kwargs):
-        self.shelf = shelf
+    def __init__(self, shelf: FlushableShelf, **kwargs):
         super().__init__(**kwargs)
+        self.shelf = shelf
 
     @property
     def _cookies(self):
@@ -19,8 +45,12 @@ class ShelvedCookieJar(http.cookiejar.CookieJar):
 
     @_cookies.setter  # type: ignore
     def _cookies(self, value):
+        if not hasattr(self, 'shelf'):
+            # bypass during initialization
+            return
         self.shelf.clear()
         self.shelf.update(value)
+        self.shelf.flush()
 
     def set_cookie(self, cookie):
         with self._cookies_lock:
@@ -28,12 +58,14 @@ class ShelvedCookieJar(http.cookiejar.CookieJar):
             d = self._cookies.setdefault(cookie.domain, {})
             d.setdefault(cookie.path, {})[cookie.name] = cookie
             self._cookies[cookie.domain] = d
+            self.shelf.flush()
 
     def clear(self, domain=None, path=None, name=None):
         super().clear(domain, path, name)
         if path is not None or name is not None:
             # Mark key as dirty.
             self._cookies[domain] = self._cookies[domain]
+        self.shelf.flush()
 
     def get(self, name, default=None):
         matches = (
