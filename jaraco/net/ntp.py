18244bcafb2771b9163be99825cd2f694a7d230c
Jason R. Coombs <jaraco@jaraco.com>
2022-12-01 15:29:35 -0500

    Wrap flaky ntp query in a couple of retries.

--- jaraco/net/ntp.py
+++ jaraco/net/ntp.py
@@ -6,10 +6,20 @@ import datetime
 
 from jaraco.text import trim
 import jaraco.logging
+import jaraco.functools
 
 log = logging.getLogger(__name__)
 
 TIME1970 = 0x83AA7E80
+
+
+_flaky = jaraco.functools.retry(
+    retries=2,
+    trap=socket.timeout,
+)
+"""
+ntp.query is flaky (by design), so be resilient to it during tests.
+"""
 
 
 def query(server, force_ipv6=False):
@@ -17,7 +27,7 @@ def query(server, force_ipv6=False):
     Return current time from NTP server as a Unix timestamp
 
     >>> import time
-    >>> res = query('us.pool.ntp.org')
+    >>> res = _flaky(query)('us.pool.ntp.org')
     >>> type(res)
     <class 'int'>
     >>> res > time.time() - 5
