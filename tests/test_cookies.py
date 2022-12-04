c49a5dde0f8cb96a7dfbe1109de7c828c0b3cf54
Jason R. Coombs <jaraco@jaraco.com>
2022-12-03 19:13:58 -0500

    Replace shelve.Shelf with a native jsonpickle-based Shelf implementation. Greatly simplifies the implementation and fixes #2.

--- tests/test_cookies.py
+++ tests/test_cookies.py
@@ -1,15 +1,15 @@
 import requests
 
-from jaraco.net.http.cookies import ShelvedCookieJar, FlushableShelf
+from jaraco.net.http.cookies import ShelvedCookieJar, Shelf
 
 
-def test_cookie_shelved(requests_mock, tmp_path, check_concurrent_dbm):
+def test_cookie_shelved(requests_mock, tmp_path):
     requests_mock.get('/', cookies={'foo': 'bar'})
     session = requests.Session()
-    cookies = tmp_path / 'cookies'
-    shelf = FlushableShelf(cookies)
+    cookies = tmp_path / 'cookies.json'
+    shelf = Shelf(cookies)
     session.cookies = ShelvedCookieJar(shelf)
     session.get('http://any/')
     assert session.cookies
 
-    assert ShelvedCookieJar(FlushableShelf(cookies))
+    assert ShelvedCookieJar(Shelf(cookies))
