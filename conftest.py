cb1ca265361911bd14e2f2c5bb4e207de04897dc
Jason R. Coombs <jaraco@jaraco.com>
2022-12-05 21:48:55 -0500

    Enable tests on Darwin

--- conftest.py
+++ conftest.py
@@ -1,6 +1,5 @@
 import sys
 import importlib
-import platform
 
 from jaraco.context import ExceptionTrap
 
@@ -19,10 +18,6 @@ collect_ignore = (
         'jaraco/net/devices/win32.py',
         'jaraco/net/devices/darwin.py',
     ]
-    + [
-        'jaraco/net/devices/__init__.py',
-    ]
-    * (platform.system() == 'Darwin')
     + [
         # modules only import on Windows
         'jaraco/net/dns.py',
