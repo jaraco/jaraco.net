0247a675e1b1ee24d2dbd2c48cb48e7955c20de6
Jason R. Coombs <jaraco@jaraco.com>
2024-02-18 10:16:47 -0500

    Replace native icmp implementation with icmplib.

--- jaraco/net/icmp.py
+++ jaraco/net/icmp.py
@@ -1,54 +1,9 @@
 import sys
 import socket
-import select
-import struct
 import time
-import operator
-import random
 import datetime
-import functools
 
-from tempora.timing import Stopwatch
-
-
-def calculate_checksum(bytes):
-    r"""
-    Calculate a 16-bit checksum on the bytes.
-
-    In particular, the 16-bit one's complement of
-    the one's complement sum of all 16-bit words
-
-    >>> calculate_checksum(b'ABCD\n')
-    31089
-    """
-    # null pad bytes to ensure 16-bit values
-    if len(bytes) % 2:
-        bytes += b'\x00'
-    n_values = len(bytes) / 2
-    values = struct.unpack('%dH' % n_values, bytes)
-    sum = functools.reduce(operator.add, values)
-    sum = (sum >> 16) + (sum & 0xFFFF)
-    sum += sum >> 16
-    return (~sum) & 0xFFFF
-
-
-def pack_echo_header(id, data, sequence=1):
-    r"""
-    Assemble an ICMP echo header
-
-    >>> pack_echo_header(1, 'echo-request')
-    b'\x08\x00\xaco\x01\x00\x01\x00'
-    >>> pack_echo_header(2, 'second-request')
-    b'\x08\x004\t\x02\x00\x01\x00'
-    """
-    if not isinstance(data, bytes):
-        data = data.encode()
-    ICMP_ECHO_REQUEST = 8
-    code = 0
-    sequence = 1
-    tmp_header = struct.pack('bbHHh', ICMP_ECHO_REQUEST, code, 0, id, sequence)
-    checksum = calculate_checksum(tmp_header + data)
-    return struct.pack('bbHHh', ICMP_ECHO_REQUEST, code, checksum, id, sequence)
+import icmplib
 
 
 def ping(dest_addr, timeout=2):
@@ -65,28 +20,10 @@ def ping(dest_addr, timeout=2):
     ...
     TimeoutError: timed out
     """
-    icmp_proto = socket.getprotobyname('icmp')
-    icmp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, icmp_proto)
-    icmp_port = 1
-    id = random.randint(0, 2**16 - 1)
-    data = struct.pack("d", time.perf_counter()) + b'Q' * 192
-    data = data[:192]
-    header = pack_echo_header(id, data)
-    packet = header + data
-    icmp_socket.sendto(packet, (dest_addr, icmp_port))
-
-    timer = Stopwatch()
-    read_fs, write_fs, ex_fs = select.select([icmp_socket], [], [], timeout)
-    delay = timer.stop()
-    if not read_fs:
+    host = icmplib.ping(dest_addr, count=1, timeout=timeout, privileged=False)
+    if not host.is_alive:
         raise socket.timeout('timed out')
-
-    packet, addr = icmp_socket.recvfrom(1024)
-    header = packet[20:28]
-    type, code, checksum, recv_id, sequence = struct.unpack('bbHHh', header)
-    if recv_id != id:
-        raise OSError(f'transmission failure ({recv_id} != {id})')
-    return delay
+    return datetime.timedelta(host.rtts[0])
 
 
 def wait_for_host(host):
